4b74f2be424571fc44faf0f2c46e7cedfa2261c5
jerome74 <walter.longo74@gmail.com>
2020-09-03 17:36:22 +0200

    message

--- src/main/kotlin/com/wlp/utubed/Util.kt
+++ src/main/kotlin/com/wlp/utubed/Util.kt
@@ -12,6 +12,7 @@ heroku create
 heroku apps:rename --app still-bastion-07273 utubed
 git add .
 git commit -m "message"
+git
 git push heroku master
-
- */
+git remote add heroku https://git.heroku.com/utubed.git
+*/
